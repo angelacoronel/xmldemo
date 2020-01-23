532130b0532dff465572674a19d1ea52e0976e1d
Jella <53487935+angelacoronel@users.noreply.github.com>
2020-01-23 11:45:59 +0800

    Update db_tracks.sql

--- db_tracks.sql
+++ db_tracks.sql
@@ -7,9 +7,9 @@ CREATE TABLE tbl_music
   `title` varchar(180) NOT NULL DEFAULT '',
   `link` varchar(180) NOT NULL DEFAULT '',
   `description` varchar(180) NOT NULL DEFAULT '',
-  `mus_title` varchar(180) NOT NULL DEFAULT '',
-  `mus_artist` varchar(180) NOT NULL DEFAULT '',
-  `mus_album` varchar(180) NOT NULL DEFAULT '',
+  `title` varchar(180) NOT NULL DEFAULT '',
+  `artist` varchar(180) NOT NULL DEFAULT '',
+  `album` varchar(180) NOT NULL DEFAULT '',
   PRIMARY KEY (`mus_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=10001 DEFAULT CHARSET=latin1;
  );
