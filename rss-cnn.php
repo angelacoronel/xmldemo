881c3325f972c4f63a6aa9030753eb0bcf1d7fef
Jella <53487935+angelacoronel@users.noreply.github.com>
2020-01-27 12:48:13 +0800

    Update rss-cnn.php

--- rss-cnn.php
+++ rss-cnn.php
@@ -1,6 +1,6 @@
 <?php
  $domOBJ = new DOMDocument();
- $domOBJ->load("email.xml");//XML page URL
+ $domOBJ->load("sample.xml");//XML page URL
  
  $content = $domOBJ->getElementsByTagName("item");
  
