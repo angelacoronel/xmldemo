4780d40c8398ba02a9cc5b1bb0bad9383ffee52b
Jella <53487935+angelacoronel@users.noreply.github.com>
2020-01-27 09:13:32 +0800

    Update db_tracks.sql

--- db_tracks.sql
+++ db_tracks.sql
@@ -1,13 +1,52 @@
-CREATE DATABASE db_tracks;
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 27, 2020 at 02:13 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
 
-USE db_tracks;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE tbl_music
- (
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_tracks`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_music`
+--
+
+CREATE TABLE `tbl_music` (
+  `mus_id` int(5) NOT NULL DEFAULT 0,
   `title` varchar(180) NOT NULL DEFAULT '',
   `genre` varchar(180) NOT NULL DEFAULT '',
   `artist` varchar(180) NOT NULL DEFAULT '',
-  `album` varchar(180) NOT NULL DEFAULT '',
-  PRIMARY KEY (`mus_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10001 DEFAULT CHARSET=latin1;
- );
+  `album` varchar(180) NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_music`
+--
+ALTER TABLE `tbl_music`
+  ADD PRIMARY KEY (`mus_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
